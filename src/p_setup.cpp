c907baf0c7cccfd8a763f480379a3a9be8ab4d5d
drfrag <drfrag666@hotmail.com>
2021-05-05 19:18:09 +0200

    Revert "- Added "clipmidtex" compatflag and applied it to unloved.pk3"
    This reverts commit b89ac1754e6858177f611ef52f6d49aef37f2234.
    
    # Conflicts:
    #	wadsrc/static/compatibility.txt

--- src/p_setup.cpp
+++ src/p_setup.cpp
@@ -2193,8 +2193,7 @@ void P_LoadLineDefs (MapData * map)
 
 		P_AdjustLine (ld);
 		P_SaveLineSpecial (ld);
-		if ((level.flags2 & LEVEL2_CLIPMIDTEX) || (ii_compatflags2 & COMPATF2_CLIPMIDTEX))
-			ld->flags |= ML_CLIP_MIDTEX;
+		if (level.flags2 & LEVEL2_CLIPMIDTEX) ld->flags |= ML_CLIP_MIDTEX;
 		if (level.flags2 & LEVEL2_WRAPMIDTEX) ld->flags |= ML_WRAP_MIDTEX;
 		if (level.flags2 & LEVEL2_CHECKSWITCHRANGE) ld->flags |= ML_CHECKSWITCHRANGE;
 	}
@@ -2280,8 +2279,7 @@ void P_LoadLineDefs2 (MapData * map)
 		P_AdjustLine (ld);
 		P_SetLineID(i, ld);
 		P_SaveLineSpecial (ld);
-		if ((level.flags2 & LEVEL2_CLIPMIDTEX) || (ii_compatflags2 & COMPATF2_CLIPMIDTEX))
-			ld->flags |= ML_CLIP_MIDTEX;
+		if (level.flags2 & LEVEL2_CLIPMIDTEX) ld->flags |= ML_CLIP_MIDTEX;
 		if (level.flags2 & LEVEL2_WRAPMIDTEX) ld->flags |= ML_WRAP_MIDTEX;
 		if (level.flags2 & LEVEL2_CHECKSWITCHRANGE) ld->flags |= ML_CHECKSWITCHRANGE;
 
